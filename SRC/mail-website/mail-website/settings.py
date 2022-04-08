35df9ef0065dc00e1fd4f00303fb5b40c64bef38
b.r <mahdis.taghizadeh1376@gmail.com>
2022-04-08 18:51:12 +0430

    settitngs

--- SRC/mail-website/mail-website/settings.py
+++ SRC/mail-website/mail-website/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from django.contrib.messages import constants as messages
 import environ
 
-
 # Initialise environment variables
 env = environ.Env()
 environ.Env.read_env()
@@ -232,11 +231,10 @@ LOGGING = {
     }
 }
 
-
 MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-secondary',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
- }
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
